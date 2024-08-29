3cee4ea31c605b6a5f2c03c9f5ac3f1481d83326
arinzehills <arinzehill@gmail.com>
2024-08-29 16:38:40 +0100

    minficay disable

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export",
+  distDir: "_next",
+  swcMinify: false,
   images: {
     domains: ["https://cloudinary.com"], // Use only the hostname
   },
