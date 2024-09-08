5d6402dc8896258516bc794dee813ed3de6779eb
arinzehills <arinzehill@gmail.com>
2024-09-08 14:01:10 +0100

    Edited and merge Nav bar

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,17 +1,14 @@
-// "use client";
-// import Product from "@/components/Product";
+"use client";
 import Nav from "@/components/Nav";
+import Product from "@/components/Product";
 import React from "react";
-
-export function generateStaticParams() {
-  return [{ id: "test" }];
-}
-
-const page = (props: any) => {
+const page = () => {
   return (
     <>
       <Nav />
-      <div className="w-full h-auto bg-white pt-44">{/* <Product /> */}</div>
+      <div className="w-full h-auto bg-white pt-44">
+        <Product />
+      </div>
     </>
   );
 };
