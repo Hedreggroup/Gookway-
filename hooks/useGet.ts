f2ec1e4300773888494abe8d8ee89019104e671c
arinzehills <arinzehill@gmail.com>
2024-10-10 13:46:44 +0100

    Hello Test Push

--- hooks/useGet.ts
+++ hooks/useGet.ts
@@ -31,7 +31,6 @@ export const useGet = <T,>(endpoint: string, runImmediately = true): UseGetResul
             console.log("DATA at Fetch", response.data)
             setData(response.data);
         } catch (err: any) {
-            console.error('ERROR', err);
             SideToast.FireError({ message: err?.response?.data?.msg || err.message });
             setError(err.message);
         } finally {
